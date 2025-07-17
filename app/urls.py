03af1ee7e20e4813412b2b8f16499cf3fd72df13
ExcessMarrow773 <atticus.falkner.walton@gmail.com>
2025-07-17 16:14:24 -0500

    added profile and media things

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('register/', views.register, name='register'),
-]
+    path('profile/', views.profile, name='profile'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
