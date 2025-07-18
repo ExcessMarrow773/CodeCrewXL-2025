fdf26a427e7ec06c93cd389e729ee9d4c9cc86a0
ExcessMarrow773 <atticus.falkner.walton@gmail.com>
2025-07-18 12:02:33 -0500

    url thing

--- app/urls.py
+++ app/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path("post/<int:pk>/", views.post_detail, name="post_detail"),
     path("category/<category>/", views.category, name="category"),
+    path("create_post/", views.create_post, name="create_post"),
+    path("create_category/", views.create_category, name="create_category"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
