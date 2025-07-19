30a33c8ed53502aca60aaa600d8c5cbbe1ff4a50
ExcessMarrow773 <atticus.falkner.walton@gmail.com>
2025-07-19 01:59:59 -0500

    added model, removed data override, removed unnecessary code

--- app/AI.py
+++ app/AI.py
@@ -24,47 +24,19 @@ class AI:
     @timer('sentiment_analysis', 'ms')
     def sentiment_analysis(self, data):
 
-        sentiment_pipeline = pipeline(modzael="nlptown/bert-base-multilingual-uncased-sentiment")
-        data = ["I love you", "I hate you"]
+        sentiment_pipeline = pipeline(model="nlptown/bert-base-multilingual-uncased-sentiment")
         
         return sentiment_pipeline(data)
     
     
 if __name__ == "__main__":
-    ai_version = True 
+    ai_version = False 
     if ai_version == False:
         ai = AI()
-        data = ["I love you", "I hate you"]
+        data = ['<p>?: (ckeditor.W001) django-ckeditor bundles CKEditor 4.22.1 which isn&#39;t supported anymore and which does have unfixed security issues, see for example https://ckeditor.com/cke4/release/CKEditor-4.24.0-LTS . You should consider strongly switching to a different editor (maybe CKEditor 5 respectively django-ckeditor-5 after checking whether the CKEditor 5 license terms work for you) or switch to the non-free CKEditor 4 LTS package. See https://ckeditor.com/ckeditor-4-support/ for more on this. (Note! This notice has been added by the django-ckeditor developers and we are not affiliated with CKSource and were not involved in the licensing change, so please refrain from complaining to us. Thanks.)</p>']
         print(ai.sentiment_analysis(data))
     if ai_version == True:
         ai = AI()
         context = ""
-        prompt = "Hi AI''"
+        prompt = "Hi AI"
         print(ai.generate_gemini_content(context, prompt).text)
-
-from django.shortcuts import render
-from app.models import Post
-from app.AI import AI
-
-def ai(request):
-    ai_response = ""
-    user_prompt = ""
-    formatted_posts = ""
-    if request.user.is_authenticated:
-        # Only get posts by the current user
-        posts = Post.objects.filter(author=request.user.username)
-        for post in posts:
-            formatted_posts += f'Title:"{post.title}" Body:"{post.body}" IMG:"{post.image.url if post.image else ""}"\n'
-
-    if request.method == "POST":
-        user_prompt = request.POST.get("prompt", "")
-        # Combine user's posts and their prompt
-        full_prompt = f"{formatted_posts}\n{user_prompt}"
-        ai_instance = AI()
-        response = ai_instance.generate_gemini_content("", full_prompt)
-        ai_response = response.text
-
-    return render(request, "app/ai_page.html", {
-        "ai_response": ai_response,
-        "user_prompt": user_prompt
-    })
